fdc6b75ef12264d6aa009df667f070b4adc7004e
kimtc99 <kimtc99@hotmail.com>
2020-11-24 02:03:14 +0900

    v 1.0.6

--- ha-flash-cc2531/run.sh
+++ ha-flash-cc2531/run.sh
@@ -7,12 +7,14 @@ IS_COORDINATOR="$(jq '.coordinator' $CONFIG_PATH)"
 IS_ZIGBEE3="$(jq '.zigbee3' $CONFIG_PATH)"
 ROUTER_LEVEL=$(jq -r '.router_level' $CONFIG_PATH)
 FLAGS="$(jq '.FLAGSs' $CONFIG_PATH)"
+
 # start server
 echo "Start flashing tool for cc2531.."
 cd /flash_cc2531
 ChipID="$(./cc_chipid $FLAGS)"
 echo $ChipID
 grepID=$(echo $ChipID | grep "ID = b524")
+grepFLAG=$(echo $FLAGS | grep "-")
 
 if [ -z "$grepID" ]; then echo "[ERROR] ChipID mismatch. Check the gpio wiring or flag options." && exit 1; fi
 
@@ -25,7 +27,7 @@ if [ "$IS_COORDINATOR" == "true" ]; then
     if [ ! -f CC2531ZNP-with-SBL.hex ]; then echo "[ERROR] Fail to download or extract. There is no hex file." && exit 1; fi
     echo "Found CC2531ZNP-with-SBL.hex. Flash firmware.."
     ./cc_erase $FLAGS
-    [ "$FLAGS" == "" ] && ./cc_write CC2531ZNP-with-SBL.hex || ./cc_write $FLAGS CC2531ZNP-with-SBL.hex
+    [ -z "$grepFLAG" ] && ./cc_write CC2531ZNP-with-SBL.hex || ./cc_write $FLAGS CC2531ZNP-with-SBL.hex
   else
     echo "Select coordinator firmware for ver 1.2"
     echo "Download and Extract a firmware.."
@@ -34,7 +36,7 @@ if [ "$IS_COORDINATOR" == "true" ]; then
     if [ ! -f CC2531ZNP-Prod.hex ]; then echo "[ERROR] Fail to download or extract. There is no hex file." && exit 1; fi
     echo "Found CC2531ZNP-Prod.hex. Flash firmware.."
     ./cc_erase $FLAGS
-    [ "$FLAGS" == "" ] && ./cc_write CC2531ZNP-Prod.hex || ./cc_write $FLAGS CC2531ZNP-Prod.hex
+    [ -z "$grepFLAG" ] && ./cc_write CC2531ZNP-Prod.hex || ./cc_write $FLAGS CC2531ZNP-Prod.hex
   fi
 else
   echo "Select router firmware.."
@@ -47,21 +49,21 @@ else
       if [ ! -f router-cc2531-std.hex ]; then echo "[ERROR] Fail to download or extract. There is no hex file." && exit 1; fi
       echo "Found router-cc2531-std.hex. Flash firmware.."
       ./cc_erase $FLAGS
-      [ "$FLAGS" == "" ] && ./cc_write router-cc2531-std.hex || ./cc_write $FLAGS router-cc2531-std.hex
+      [ -z "$grepFLAG" ] && ./cc_write router-cc2531-std.hex || ./cc_write $FLAGS router-cc2531-std.hex
       ;;
     1)
       echo "ROUTER: firmware with additional diagnostic messages"
       if [ ! -f router-cc2531-diag.hex ]; then echo "[ERROR] Fail to download or extract. There is no hex file." && exit 1; fi
       echo "Found router-cc2531-diag.hex. Flash firmware.."
       ./cc_erase $FLAGS
-      [ "$FLAGS" == "" ] && ./cc_write router-cc2531-diag.hex || ./cc_write $FLAGS router-cc2531-diag.hex
+      [ -z "$grepFLAG" ] && ./cc_write router-cc2531-diag.hex || ./cc_write $FLAGS router-cc2531-diag.hex
       ;;
     2)
       echo "ROUTER: firmware with additional diagnostic messages and USB support"
       if [ ! -f router-cc2531-diag-usb.hex ]; then echo "[ERROR] Fail to download or extract. There is no hex file." && exit 1; fi
       echo "Found router-cc2531-diag-usb.hex. Flash firmware.."
       ./cc_erase $FLAGS
-      [ "$FLAGS" == "" ] && ./cc_write router-cc2531-diag-usb.hex || ./cc_write $FLAGS router-cc2531-diag-usb.hex
+      [ -z "$grepFLAG" ] && ./cc_write router-cc2531-diag-usb.hex || ./cc_write $FLAGS router-cc2531-diag-usb.hex
       ;;
     *)
       echo "Unknown router_level. Choose 0, 1 or 2."
